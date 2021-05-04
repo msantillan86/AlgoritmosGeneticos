239b84201cd717f2a841e5a5499847648573ce4c
Matias Alejandro Santillan <SantillanM@sancristobal.com.ar>
2021-05-04 18:02:42 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var random = new Random();
+            var frase = "Si los PERROS LADRAN, Sancho, es porque CABALGAMOS!";
+            
+            Console.WriteLine(frase);
+            var ga = new GeneticAlgoritm(frase, random);
+
+            while (ga.BestFitness < 1)
+            {
+                ga.NewGeneration();
+                Console.WriteLine($"{ga.Generation} > {new string(ga.BestGenes)} : {ga.BestFitness}%");
+            }
+        }
+    }
+}
